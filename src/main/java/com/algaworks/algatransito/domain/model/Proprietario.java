d4104f9fd06bc6228b54e18f5002059ec91de761
William Domingues <williamcdzx88@gmail.com>
2025-11-03 15:39:18 -0300

    feat: atualização dos endpoints de proprietários

---
+++ src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -0,0 +1,42 @@
+package com.algaworks.algatransito.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode (onlyExplicitlyIncluded = true)
+@Entity
+//@Table(name = "tb_proprietario") se tabela fosse diferente do nome da class deverá se chamado este @
+public class Proprietario {
+
+    // @id indentificação da chave primaria
+    //@GeneratedValue define a estrategia de geração da chave para usar de forma nativa do banco de dados
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+
+    @NotBlank
+    @Size(max = 60)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 255)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "fone")
+    private String telefone;
+
+
+
+}
